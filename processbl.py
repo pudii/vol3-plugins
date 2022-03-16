69bf9330a7978eb9ac44c307cc2f62ec5001c406
patrickr <pudii@users.noreply.github.com>
2022-03-16 22:40:34 +0100

    Update license name

--- processbl.py
+++ processbl.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2022
-#     Authors: Pudii (2022), Csaba Barta (2015)
+#     Authors: Patrick Reichenberger (2022), Csaba Barta (2015)
 #  
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
